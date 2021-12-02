d9c851cf43f0661cb012dc2a81928081b4afc48f
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2021-12-02 17:25:37 +0100

    Jonatan commit: User.java Updated SignUpController.java Created SignUPServices.java Created UserRepository.java Updated

--- src/main/java/com/kea/planit/services/SignUPServices.java
+++ src/main/java/com/kea/planit/services/SignUPServices.java
@@ -1,8 +1,15 @@
 package com.kea.planit.services;
 
+import com.kea.planit.models.User;
+import com.kea.planit.repositories.UserRepository;
+
 public class SignUPServices {
 
-    public boolean isUserValid(){
+
+    public boolean isUserValid(User user){
+        if(UserRepository.getInstance().getAllUsers().contains(user)){
+            return true;
+        }
         return false;
     }
 }
