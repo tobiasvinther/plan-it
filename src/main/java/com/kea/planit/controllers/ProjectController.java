2959b579ca0e3a7374a208dcf9b158e14f9f62a3
Thony <thon0101@stud.kea.dk>
2021-12-03 11:34:51 +0100

    commit

--- src/main/java/com/kea/planit/controllers/ProjectController.java
+++ src/main/java/com/kea/planit/controllers/ProjectController.java
@@ -3,16 +3,19 @@ package com.kea.planit.controllers;
 import com.kea.planit.repositories.ProjectRepository;
 import com.kea.planit.repositories.TaskRepository;
 import com.kea.planit.services.ProjectService;
+import com.kea.planit.services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProjectController {
-
+    //Instance singleton
     TaskRepository taskRepository = new TaskRepository();
     ProjectRepository projectRepository = new ProjectRepository();
     ProjectService projectService = new ProjectService();
+    TaskService taskService = new TaskService();
+
 
     @GetMapping("/")
     public String index() {
@@ -23,9 +26,9 @@ public class ProjectController {
         projectModel.addAttribute("projectList", projectRepository.getProjectList());
         projectModel.addAttribute("taskList", taskRepository.getTaskList());
         //Add total hour calculation to all projects.
-        projectModel.addAttribute("totalHours", projectService.calculateProjectHours(projectRepository.getProjectList()));
+        projectModel.addAttribute("totalProjectsHours", projectService.calculateProjectHours(projectRepository.getProjectList()));
         //Add total hour calculation to each projects.
-
+        projectModel.addAttribute("totalTaskHours", taskService.calculateTaskHours(taskRepository.getTaskList()));
         return "view-project";
 
     }
--- src/main/java/com/kea/planit/services/TaskService.java
+++ src/main/java/com/kea/planit/services/TaskService.java
@@ -1,4 +1,15 @@
 package com.kea.planit.services;
 
+import com.kea.planit.models.Task;
+
+import java.util.ArrayList;
+
 public class TaskService {
+    public int calculateTaskHours(ArrayList<Task> taskList) {
+        int totalHours = 0;
+        for (Task task : taskList) {
+            totalHours += task.getHours();
+        }
+        return totalHours;
+    }
 }
