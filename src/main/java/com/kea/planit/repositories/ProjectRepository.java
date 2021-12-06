dde10758de63614d0f0d62a1ab22711a7f3eb6ef
Thony <thon0101@stud.kea.dk>
2021-12-06 11:03:30 +0100

    commit

--- src/main/java/com/kea/planit/repositories/ProjectRepository.java
+++ src/main/java/com/kea/planit/repositories/ProjectRepository.java
@@ -1,7 +1,11 @@
 package com.kea.planit.repositories;
 
 import com.kea.planit.models.Project;
+import com.kea.planit.utilities.DBconnector;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -17,5 +21,28 @@ public class ProjectRepository {
         projectList.add(project2);
         projectList.add(project3);
         return projectList;
+    }
+    public ArrayList getAllProjects(int id) {
+        try {
+            Connection connection = DBconnector.getConnection();
+            PreparedStatement ppst = connection.prepareStatement("SELECT * FROM projects WHERE id=" + id);
+            ResultSet rs = ppst.executeQuery();
+            ArrayList<Project> allProjectList = new ArrayList<>();
+            while (rs.next()) {
+                int projectid = rs.getInt(1);
+                String projectname = rs.getString(2);
+                int userid = rs.getInt(3);
+                Project projectList = new Project(projectid, projectname, userid);
+                allProjectList.add(projectList);
+                System.out.println(projectList);
+            }
+            System.out.println("returned projectlist");
+            return allProjectList;
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
     }
 }
