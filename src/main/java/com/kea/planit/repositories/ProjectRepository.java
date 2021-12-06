7edd142da60854c49f65ac9f11fb9ad5a65f77bd
Thony <thon0101@stud.kea.dk>
2021-12-06 13:08:06 +0100

    commit

--- src/main/java/com/kea/planit/repositories/ProjectRepository.java
+++ src/main/java/com/kea/planit/repositories/ProjectRepository.java
@@ -3,10 +3,7 @@ package com.kea.planit.repositories;
 import com.kea.planit.models.Project;
 import com.kea.planit.utilities.DBconnector;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -27,29 +24,29 @@ public class ProjectRepository {
         return projectList;
     }*/
     public ArrayList viewProject(int id) {
+        ArrayList<Project> allProjectList = new ArrayList<>();
         try {
             Connection connection = DBconnector.getConnection();
-            PreparedStatement ppst = connection.prepareStatement("SELECT * FROM projects WHERE id=" + id);
+            PreparedStatement ppst = connection.prepareStatement("SELECT * FROM projects WHERE project_owner=");
             ResultSet rs = ppst.executeQuery();
-            ArrayList<Project> allProjectList = new ArrayList<>();
+            allProjectList = new ArrayList<>();
             while (rs.next()) {
                 Project project = new Project(
-                rs.getInt("id"),
-                rs.getString("name"),
-                rs.getString("description"),
-                rs.getInt("hoursinall"),
-                rs.getString("status"),
-                rs.getDate("deadline")
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("description"),
+                        rs.getInt("hoursinall"),
+                        rs.getString("status"),
+                        rs.getDate("deadline")
                 );
                 allProjectList.add(project);
             }
-            System.out.println("returned projectlist");
-            return allProjectList;
-
-
-        } catch (Exception e) {
-            System.out.println(e);
-            return null;
+        } catch (SQLException e) {
+            System.out.println("Something went wrong when fetching tasks from database");
+            System.out.println(e.getMessage());
         }
+        System.out.println("returned projectlist");
+        return allProjectList;
     }
-}
+    }
+
