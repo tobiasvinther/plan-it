a6672a58b88222e16b5c519f6ff3ee000e880cbb
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2021-12-08 23:29:29 +0100

    Jonatan commit: Login stuff - needs fixing, errors works

--- src/main/java/com/kea/planit/services/AuthUserDetailService.java
+++ src/main/java/com/kea/planit/services/AuthUserDetailService.java
@@ -1,8 +1,9 @@
 package com.kea.planit.services;
 
-import com.kea.planit.models.User;
+import com.kea.planit.models.UserModel;
 import com.kea.planit.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,23 +11,27 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthUserDetailService implements UserDetailsService {
-    private UserRepository UR;
+
+    private final UserRepository UR;
 
     @Autowired
-    public AuthUserDetailService(UserRepository userRepository){
-        this.UR = userRepository;
+    public AuthUserDetailService(UserRepository UR){
+        this.UR = UR;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
-        final User user = UR.findByEmail(email);
-        if( user == null){
+        final UserModel userModel = UR.findByEmail(email);
+        System.out.println(userModel);
+        if( userModel == null){
+            System.out.println("Here");
             throw new UsernameNotFoundException(email);
         }
-        UserDetails userDetails = org.springframework.security.core.userdetails.User
-                .withUsername(user.getEmail())
-                .password(user.getPassword())
+        UserDetails userDetails = User
+                .withUsername(userModel.getEmail())
+                .password(userModel.getPassword())
                 .authorities("USER").build();
+        System.out.println(userDetails);
         return userDetails;
     }
 
--- src/main/java/com/kea/planit/utilities/SpringSecurityConfig.java
+++ src/main/java/com/kea/planit/utilities/SpringSecurityConfig.java
@@ -1,13 +1,12 @@
 package com.kea.planit.utilities;
 
 
-import com.kea.planit.models.User;
+import com.kea.planit.services.AuthUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,41 +21,47 @@ import javax.annotation.Resource;
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Resource
-    private UserDetailsService userDetailsService;
+    private AuthUserDetailService userDetailsService;
+
+    @Autowired
+    public SpringSecurityConfig(AuthUserDetailService authUserDetailService){
+        this.userDetailsService = authUserDetailService;
+    }
+
 
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    protected void configure(final AuthenticationManagerBuilder auth, User user) throws Exception{
-        auth.inMemoryAuthentication().
-                withUser(user.getEmail()).password(passwordEncoder.encode(user.getPassword())).roles("USER");
+    @Bean
+    public DaoAuthenticationProvider authProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.authenticationProvider(authProvider());
+    }
+
 
     @Override
     protected void configure(HttpSecurity httpSecurity)throws Exception{
         httpSecurity.csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/","/sign-up","/login").permitAll()
-                .antMatchers("/account/**").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/","/sign-up","/login")
+                .permitAll()
+                .antMatchers("/view-tasks").access("hasRole('ROLE_ADMIN')")
                 .and()
                 .formLogin(form -> form
                         .loginPage("/login")
                         .defaultSuccessUrl("/view-tasks")
                         .failureUrl("/login?error=true")
                 );
-    }
-    @Bean
-    public DaoAuthenticationProvider authProvider(){
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder);
-        return authProvider;
     }
 
 }
