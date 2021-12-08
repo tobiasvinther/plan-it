edd21a0f11569b33b214b113314c97bc9eaf3102
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2021-12-09 00:37:16 +0100

    Jonatan commit: SpringSecurityConfig.java updated configure -Now allows user to view View-tasks -Removed .csrf an .disable
    UserRepository.java
    -Added PasswordEncoder
    -Encodes passwords in repository
    -Updated findByEmail

--- src/main/java/com/kea/planit/repositories/UserRepository.java
+++ src/main/java/com/kea/planit/repositories/UserRepository.java
@@ -1,12 +1,16 @@
 package com.kea.planit.repositories;
 
 import com.kea.planit.models.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
 @Repository
 public class UserRepository {
+
     private static UserRepository singe_UR = null;
     private static ArrayList<UserModel> userModels = new ArrayList<>();
     public static UserRepository getInstance(){
@@ -16,10 +20,13 @@ public class UserRepository {
         return singe_UR;
     }
 
+    private static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+
     //Dummy Class for now
     public ArrayList<UserModel> getAllUsers(){
-        UserModel testUserModel = new UserModel("Test","Test@mail.com","Test123");
-        UserModel secondUserModel = new UserModel("Test2","Test2@mail.com","Test231");
+        UserModel testUserModel = new UserModel("Test","Test@mail.com",passwordEncoder.encode("Test123"));
+        UserModel secondUserModel = new UserModel("Test2","Test2@mail.com",passwordEncoder.encode("Test321"));
         userModels.add(testUserModel);
         userModels.add(secondUserModel);
         return userModels;
@@ -32,7 +39,9 @@ public class UserRepository {
     public UserModel findByEmail(String email){
         for(UserModel u: getAllUsers()){
             System.out.println("Do I get here?");
-            if(u.getEmail().contains(email)){
+            System.out.println(email+" "+u.getEmail());
+            System.out.println(u.getEmail().contains(email)?true:false);
+            if(u.getEmail().equals(email)){
                 System.out.println("I am here");
                 return u;
             }
