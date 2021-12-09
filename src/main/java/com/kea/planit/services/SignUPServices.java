f468f23f61f88682538a6f103f8afbb97255d1ab
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2021-12-09 17:18:17 +0100

    Jonatan commit: SignUPServices.java -Added passwordEncoder

--- src/main/java/com/kea/planit/services/SignUPServices.java
+++ src/main/java/com/kea/planit/services/SignUPServices.java
@@ -3,12 +3,12 @@ package com.kea.planit.services;
 import com.kea.planit.models.UserModel;
 import com.kea.planit.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class SignUPServices {
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+    private static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public boolean isUserValid(UserModel userModel){
         return isEmailValid(userModel.getEmail());
